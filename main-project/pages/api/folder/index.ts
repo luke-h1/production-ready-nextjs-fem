35ac0071477f6fdc78fa3fb1bcbfe649fd26fea3
luke-h1 <luke.howsam@yahoo.com>
2021-01-09 12:51:24 +0000

    ready for deploy

---
+++ main-project/pages/api/folder/index.ts
@@ -0,0 +1,20 @@
+import nc from 'next-connect'
+import middleware from '../../../middleware/all'
+import { folder } from '../../../db'
+import onError from '../../../middleware/error'
+
+const handler = nc({
+  onError,
+})
+
+handler.use(middleware)
+
+handler.post(async (req, res) => {
+  const newFolder = await folder.createFolder(req.db, {
+    ...req.body,
+    createdBy: req.user.id,
+    name: req.body.name,
+  })
+  res.send({ data: newFolder })
+})
+export default handler
